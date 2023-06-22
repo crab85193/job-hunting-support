adf035d654b80af9294e3b7163fadf8951da08fc
CrabAnderson <crab85193@gmail.com>
2023-06-22 14:22:30 +0900

    first commit

---
+++ job-hunting-support/job_hunting_supportApp.swift
@@ -0,0 +1,17 @@
+//
+//  job_hunting_supportApp.swift
+//  job-hunting-support
+//
+//  Created by Crab Anderson on 2023/06/22.
+//
+
+import SwiftUI
+
+@main
+struct job_hunting_supportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
